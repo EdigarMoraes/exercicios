d82d1fd6bc9f734b452f8c507837694355f0b9db
Edigar Caetano Moraes <113651285+EdigarMoraes@users.noreply.github.com>
2023-01-11 13:46:38 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var idade = 44;
+var altura = 1.78;
+var nome = "Edigar";
+var pagou = true;
+
+console.log("Os valores atribuidos as variáveis são:");
+console.log("Idade: ", idade);
+console.log("Altura ", altura);
+console.log("Nome ", nome);
+console.log("Pagou ", pagou);
+
+console.log("Os valores das variáveis são: ");
+console.log("idade ", typeof (idade));
+console.log("altura ", typeof (altura));
+console.log("nome ", typeof (nome));
+console.log("pagou ", typeof (pagou));
+
+
